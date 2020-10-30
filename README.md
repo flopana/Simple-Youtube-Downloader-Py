1b970812a7485d41237ef2c4da28999612eada99
Florian Erbs <florian@f-erbs.de>
2020-10-30 21:03:07 +0100

    skrrrr

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Simple-Youtube-Downloader-Py
+#Simple-Youtube-Downloader-Py
+
+## Installation
+```
+Chocolatey:
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+ffmpeg:
+choco install ffmpeg
+
+gsudo:
+choco install gsudo
+
+pip install -r requirements.txt
+``` 
+
+## Troubleshooting
+
+Update ausführen oder mach en issue auf
--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import os
 
 def download(ydl_opts):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v=m8j58_AaLfw'])
+        link = input("Link:\n")
+        ydl.download([link])
+        menu()
 
 
 def update():
     os.system('git checkout master')
     os.system('git pull')
     os.system('pip install --upgrade youtube-dl')
-    os.system('choco upgrade ffmpeg -y')
+    os.system('gsudo choco upgrade ffmpeg -y')
+    os.system('cls')
     menu()
 
 
@@ -35,7 +38,7 @@ def menu():
         download(ydl_opts)
     elif option == 2:
         ydl_opts = {
-            'format': 'bestvideo/best',
+            'format': 'best',
             'outtmpl': 'video/%(title)s.%(ext)s',
             'postprocessors': [{
                 'key': 'FFmpegVideoConvertor',
